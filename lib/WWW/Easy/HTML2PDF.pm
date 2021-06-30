f1b1ff0b3ee8608de26ad2d27e8a3fae4f2f8af0
Ivan Panchenko <wao@mail.ru>
2021-06-30 11:47:21 +0300

    parameters to pdf coverter

--- lib/WWW/Easy/HTML2PDF.pm
+++ lib/WWW/Easy/HTML2PDF.pm
@@ -31,7 +31,14 @@ sub html2pdf {
 	my ($html, $pdf, %opt)= @_;
 	my $err;
 	my $converter = $opt{converter} || init_converter();
-	run [$converter, ($opt{with_js} ? ( '--enable-javascript', '--javascript-delay','10000', '--no-stop-slow-scripts') : ()),
+	my @add_options;
+	if(my $m = $opt{margins}) { 
+		push @add_options, "-T", $m->[0], "-R", $m->[1], "-B", $m->[2], "-L", $m->[3];
+	}
+	if(my $m = $opt{page_numbers}) { # left,right or center
+		push @add_options, "--footer-$m",'[page]/[topage]';
+	}
+	run [$converter, ($opt{with_js} ? ( '--enable-javascript', '--javascript-delay','10000', '--no-stop-slow-scripts') : ()), @add_options,
 		 '-q', '-', '-'], $html, $pdf, \$err
   		 or die "cat returned $? stderr was: $err";
 	if(ref($pdf)) { 
